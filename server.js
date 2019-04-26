e358058a361eaac68bbe1ca5a42e8a2d4eec1f80
Mily <milycrn26@gmail.com>
2019-04-25 21:28:29 -0400

    Primer Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials/');
+// Express HBS engine
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'David',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about', {
+
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
